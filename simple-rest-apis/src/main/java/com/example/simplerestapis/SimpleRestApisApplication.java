679737db831b2e71a52281609499720942ac33f2
velurubharath <velurubharath@gmail.com>
2020-09-06 12:27:22 +0530

    [simple-rest-apis]

---
+++ simple-rest-apis/src/main/java/com/example/simplerestapis/SimpleRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplerestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApisApplication.class, args);
+	}
+
+}
