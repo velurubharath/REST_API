97377fe17ecf9da564544f1a796214176454e8b7
velurubharath <velurubharath@gmail.com>
2020-09-06 10:39:55 +0530

    [Uploading Demo class]

---
+++ spring-boot-example-sts/src/test/java/com/javatpoint/spring_boot_example_sts/Demo.java
@@ -0,0 +1,11 @@
+package com.javatpoint.spring_boot_example_sts;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Demo class");
+	}
+
+}
